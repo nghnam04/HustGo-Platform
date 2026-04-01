7a34c374610cf4f5fc9110646870aba82be73542
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2026-04-02 00:37:00 +0700

    feat: implement config server

---
+++ config-server/src/main/java/vn/edu/hust/config_server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hust.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
